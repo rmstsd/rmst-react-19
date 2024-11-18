9484f5beb3333e98418eee56b82bbe28059d5e99
rmst <rmstsd@qq.com>
2024-11-18 22:01:53 +0800

    d

--- packages/playground/src/index.tsx
+++ packages/playground/src/index.tsx
@@ -3,8 +3,13 @@ import App from './App'
 
 import './main.less'
 
+import d from 'rmst-design'
+console.log('d', d)
+
 const rootEl = document.getElementById('root')
 if (rootEl) {
   const root = ReactDOM.createRoot(rootEl)
   root.render(<App />)
 }
+
+// 15026583648
